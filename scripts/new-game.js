f1aca14994f4fdb7994a8968894829ee7344edbf
Nathan Cochran <NathanJCochran@gmail.com>
2014-11-08 17:01:44 -0500

    Even better player selection

--- scripts/new-game.js
+++ scripts/new-game.js
@@ -3,13 +3,4 @@ $(document).ready(function() {
         limit:4,
         show_label:true
     });
-
-// Masonry?    
-//    var container = $("select.player-select").next("ul.thumbnails");
-//    container.imagesLoaded(function(){
-//        container.masonry({
-//            itemSelector:"li",
-//            columnWidth:400
-//        });
-//    });
 });
